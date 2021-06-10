a94105c7b1267a8c045c11d9fc48f0e4b3e67f03
briansleonel <gonzalezbrianleonel76@gmail.com>
2021-06-09 23:16:01 -0300

    Busqueda en backend finalizada

--- src/app/components/petshop/catalogo/catalogo.component.ts
+++ src/app/components/petshop/catalogo/catalogo.component.ts
@@ -11,31 +11,38 @@ export class CatalogoComponent implements OnInit {
 
   productos: Array<Producto>;
   page:number;
+  findNameProd : string;
+
   constructor(
     private productoService: ProductoService,
 
   ) { }
 
   ngOnInit(): void {
+    this.findNameProd = '';
     this.cargarCatalogoProductos();
   }
 
   cargarCatalogoProductos():void{
     this.productos = new Array<Producto>();
-    this.productoService.getProductos().subscribe(
+    this.productoService.get(this.findNameProd).subscribe(
       result=>{
         result.forEach(element => {
           let producto = new Producto();
           Object.assign(producto, element);
           this.productos.push(producto);
-          console.log(element);
-          console.log(producto)
         });
+        console.log(this.productos)
       },
       error=>{
         console.log(error);
       }
     )
+  }
+
+  cleanFilters(): void {
+    this.findNameProd = '';
+    this.cargarCatalogoProductos();
   }
 
 }
--- src/app/services/producto/producto.service.ts
+++ src/app/services/producto/producto.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Producto } from 'src/app/models/producto/producto';
 
 @Injectable({
   providedIn: 'root'
@@ -18,6 +19,17 @@ export class ProductoService {
       params: new HttpParams({})
     }
     return this.http.get(this.urlBase,options);
+  }
+
+  get(nombreProd: string):Observable<any>{
+    const httpOptions = {
+      headers: new HttpHeaders(),
+      params : {
+        nombre : nombreProd
+      }
+    }
+
+    return this.http.get(this.urlBase, httpOptions)
   }
 
   getProducto(id:string):Observable<any>{
